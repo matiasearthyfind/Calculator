bbb76680713989308189d8054a36004a112df56d
matiasearthyfind <mearthyfind11@gmail.com>
2020-04-01 14:26:39 +0200

    Committing Sub function to main

---
+++ Calculator/src/calculator/operations/Subtraction.java
@@ -0,0 +1,23 @@
+package calculator.operations;
+
+public class Subtraction extends Operation {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOperator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double calc() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
