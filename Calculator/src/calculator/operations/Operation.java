3f16e2346e765602f73e39d08c7d12c60488bbcc
matiasearthyfind <mearthyfind11@gmail.com>
2020-04-01 14:19:52 +0200

    committing code we have so far for Calculator

---
+++ Calculator/src/calculator/operations/Operation.java
@@ -0,0 +1,61 @@
+package calculator.operations;
+
+public abstract class Operation {
+
+    protected double a, b;
+
+    /**
+     * Returns the name of the operation
+     *
+     * @return the name
+     */
+
+    public abstract String getName();
+
+    /**
+     * Returns the operator needed to trigger the operation
+     *
+     * @return the operator
+     */
+
+    public abstract String getOperator();
+
+    /**
+     * Returns the actual computation and returns the result
+     *
+     * @return the result of the operation
+     */
+
+    public abstract double calc();
+
+    /**
+     * Sets the two numbers to perform the operation
+     *
+     * @param a first number
+     * @param b second number
+     */
+
+    public void setNumbers(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+
+    /**
+     * Parses the provided string to return corresponding operator
+     *
+     * @param o the string containing the operator
+     * @return the actual operation
+     */
+
+    public static Operation parseOperator(String o) {
+        Operation[] ops = new Operation[]{ new Addition(), new Multiplication() , new Subtraction() };
+        for (Operation op : ops) {
+            if (o.equals(op.getOperator())) {
+                return op;
+            }
+        }
+        return null;
+    }
+
+}
