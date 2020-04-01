d5f6043755080be2999aea6d49bc5cb3db576902
matiasearthyfind <mearthyfind11@gmail.com>
2020-04-01 14:31:16 +0200

    commit to main

--- Calculator/src/calculator/operations/Subtraction.java
+++ Calculator/src/calculator/operations/Subtraction.java
@@ -1,23 +1,18 @@
 package calculator.operations;
 
 public class Subtraction extends Operation {
+    @Override
+    public String getName() {
+        return "subtraction";
+    }
 
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getOperator() {
+        return "-";
+    }
 
-	@Override
-	public String getOperator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public double calc() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+    @Override
+    public double calc() {
+        return a - b;
+    }
 }
